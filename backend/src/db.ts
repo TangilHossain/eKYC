10b41de697ab8136e0fb31814e57b04caf356d0e
tangilhossain <shawonshagor0@gmail.com>
2025-11-12 00:11:41 +0600

    Test

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
-    console.log("✅ MongoDB connected");
+    console.log("MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err);
   }
